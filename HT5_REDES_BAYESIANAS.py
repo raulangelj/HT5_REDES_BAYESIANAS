e5ad0a4da0cf670f493c180de5286c0713524fb4
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-03-18 20:45:23 -0600

    some

--- HT5_REDES_BAYESIANAS.py
+++ HT5_REDES_BAYESIANAS.py
@@ -35,7 +35,11 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 from scipy.stats import normaltest
 from sklearn.linear_model import Ridge
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix
 from yellowbrick.regressor import ResidualsPlot
+from sklearn.metrics import make_scorer, accuracy_score, precision_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 # import sklearn.mixture as mixture
 # from sklearn import datasets
 # from sklearn.cluster import DBSCAN
@@ -54,14 +58,132 @@ plt.style.use('ggplot')
 
 # %% [markdown]
 # ## 1. Use los mismos conjuntos de entrenamiento y prueba que utilizó en las dos hojas anteriores.
+
+# %%
+train = pd.read_csv('./train.csv', encoding='latin1')
+train.head()
+
+# %%
+usefullAttr = ['SalePrice', 'LotArea', 'OverallCond', 'YearBuilt', 'MasVnrArea', 'TotalBsmtSF', '1stFlrSF',
+               '2ndFlrSF', 'GrLivArea', 'TotRmsAbvGrd', 'GarageCars', 'WoodDeckSF', 'OpenPorchSF', 'EnclosedPorch', 'PoolArea', 'Neighborhood', 'OverallQual']
+
+
+# %%
+data = train[usefullAttr]
+data.head()
+
+# %%
+sns.pairplot(data[['SalePrice', 'LotArea', 'TotalBsmtSF',
+             'GrLivArea', 'TotRmsAbvGrd', 'OverallQual']], hue='SalePrice')
+plt.show()
+
+# %%
+# NORMALIZAMOS DATOS
+if 'Neighborhood' in data.columns:
+    usefullAttr.remove('Neighborhood')
+data = train[usefullAttr]
+X = []
+for column in data.columns:
+    try:
+        column
+        if column != 'Neighborhood' or column != 'SalePrice':
+            data[column] = (data[column]-data[column].mean()) / \
+                data[column].std()
+            X.append(data[column])
+    except:
+        continue
+data_clean = data.dropna(subset=usefullAttr, inplace=True)
+X_Scale = np.array(data)
+X_Scale
+
+# %%
+kmeans = cluster.KMeans(n_clusters=3)
+kmeans.fit(X_Scale)
+kmeans_result = kmeans.predict(X_Scale)
+kmeans_clusters = np.unique(kmeans_result)
+for kmeans_cluster in kmeans_clusters:
+    # get data points that fall in this cluster
+    index = np.where(kmeans_result == kmeans_cluster)
+    # make the plot
+    plt.scatter(X_Scale[index, 0], X_Scale[index, 1])
+plt.show()
+
+# %%
+data['cluster'] = kmeans.labels_
+print(data[data['cluster'] == 0].describe().transpose())
+print(data[data['cluster'] == 1].describe().transpose())
+print(data[data['cluster'] == 2].describe().transpose())
+# ## Variable clasificacion
+# %%
+# Clasificacion de casas en: Economias, Intermedias o Caras.
+data.fillna(0)
+limit1 = data.query('cluster == 0')['SalePrice'].mean()
+limit2 = data.query('cluster == 1')['SalePrice'].mean()
+data['Clasificacion'] = data['LotArea']
+data.loc[data['SalePrice'] < limit1, 'Clasificacion'] = 'Economica'
+data.loc[(data['SalePrice'] >= limit1) & (
+    data['SalePrice'] < limit2), 'Clasificacion'] = 'Intermedia'
+data.loc[data['SalePrice'] >= limit2, 'Clasificacion'] = 'Caras'
+
+# %% [markdown]
+# #### Contamos la cantidad de casas por clasificacion
+
+# %%
+# Obtener cuantos datos hay por cada clasificacion
+print(data['Clasificacion'].value_counts())
+
+# %% [markdown]
+# ## Dividmos en entrenamiento y prueba
+
+# %% [markdown]
+# # Estableciendo los conjuntos de Entrenamiento y Prueba
+
+# %%
+y = data['Clasificacion']
+X = data[['SalePrice', 'LotArea', 'TotalBsmtSF',
+          'GrLivArea', 'TotRmsAbvGrd', 'OverallQual']]
+
+# %%
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, train_size=0.7)
+y_train
+
+# %% [markdown]
+# 70% de entrenamiento y 30% prueba
+
+# %%
+X_train.info()
+
+# %%
+X_test.info()
+
 # %% [markdown]
 # ## 2. Elabore un modelo de bayes ingenuo (naive bayes) utilizando el conjunto de entrenamiento y explique los resultados a los que llega. El experimento debe ser reproducible por lo que debe  fijar  que  los  conjuntos  de  entrenamiento  y  prueba  sean  los  mismos  siempre  que  se ejecute el código.
+
+# %% [markdown]
+# ## Creando el modelo
+
+# %%
+gaussian = GaussianNB()
+gaussian.fit(X_train, y_train)
+
 # %% [markdown]
 # ## 3. El modelo debe ser de clasificación, use la variable categórica que hizo con el precio de las casas (barata, media y cara) como variable respuesta.
+# %%
+y_pred = gaussian.predict(X_test)
 # %% [markdown]
 # ## 4. Utilice  el  modelo  con  el  conjunto  de  prueba  y  determine  la  eficiencia  del  algoritmo  para clasificar.
+# %%
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred, average='micro')
+recall = recall_score(y_test, y_pred, average='micro')
+f1 = f1_score(y_test, y_pred, average='micro')
+print('Accuracy: ', accuracy)
 # %% [markdown]
 # ## 5. Haga  un  análisis  de  la  eficiencia  del  algoritmo  usando  una  matriz  de  confusión.  Tenga  en cuenta la efectividad, donde el algoritmo se equivocó más, donde se equivocó menos y la importancia que tienen los errores.
+
+cm = confusion_matrix(y_test, y_pred)
+print('Confusion matrix for Naive Bayes\n', cm)
 # %% [markdown]
 # ## 6. Analice el modelo. Explique si hay sobreajuste (overfitting) o no.
 # %% [markdown]
