847fcf59f5e92689a26c54495850d9dabde3e66f
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-03-18 18:25:12 -0600

    initial commit

---
+++ HT5_REDES_BAYESIANAS.py
@@ -0,0 +1,10 @@
+# %% [markdown]
+# # HOJA DE TRABAJO 5 REDES BAYESIANAS
+
+# Raul Jimenez 19017
+
+# Donaldo Garcia 19683
+
+# Oscar Saravia 19322
+
+# link al repo: https://github.com/raulangelj/HT5_REDES_BAYESIANAS
